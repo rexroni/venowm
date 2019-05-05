eca017ae3c97edc6a11feb5f7ba1336c4b55d44f
Rex Roni <rexroni7-general@yahoo.com>
2019-05-05 15:45:11 -0700

    now rendering and receiving key and mouse events

--- backend_wlroots.c
+++ backend_wlroots.c
@@ -1,11 +1,17 @@
+#define _POSIX_C_SOURCE 199309L
 #include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
 #include <wayland-server.h>
 
 #include <wlr/backend.h>
 #include <wlr/types/wlr_compositor.h>
+#include <wlr/types/wlr_matrix.h>
 #include <wlr/types/wlr_xdg_shell.h>
 #include <wlr/interfaces/wlr_output.h>
 #include <wlr/render/interface.h>
+
+#include <xkbcommon/xkbcommon.h>
 
 #include "backend.h"
 #include "venowm.h"
@@ -38,18 +44,19 @@ struct be_screen_t {
     struct wl_listener frame_listener;
 };
 
-struct be_window_t {
-};
-
 struct backend_t {
     struct wl_display *display;
     struct wl_event_loop *loop;
     // the wayland backend
     struct wlr_backend *wlr_backend;
+    struct wl_listener new_input_device;
     // the wlr compositor
     struct wlr_compositor *compositor;
+    struct wl_listener wlr_surface_created;
+    struct wl_listener compositor_destroyed_listener;
     // shell interfaces
-    struct wlr_xdg_shell_v6 *xdg_shell_v6;
+    struct wlr_xdg_shell *xdg_shell;
+    struct wl_listener xdg_shell_new_listener;
     // outputs
     struct wl_listener new_output_listener;
     struct wl_list be_screens;
@@ -73,13 +80,20 @@ static void handle_output_destroyed(struct wl_listener *l, void *data){
     (void)data;
     be_screen_t *be_screen = wl_container_of(l, be_screen,
                                              output_destroyed_listener);
+    backend_t *be = be_screen->be;
+
     be_screen_free(be_screen);
+
+    // if that was the last screen, close venowm
+    if(be->be_screens.next == &be->be_screens){
+        backend_stop(be);
+    }
 }
 
 static void handle_frame(struct wl_listener *l, void *data){
     (void)data;
-    be_screen_t *be_screen = wl_container_of(l, be_screen,
-                                             output_destroyed_listener);
+    be_screen_t *be_screen = wl_container_of(l, be_screen, frame_listener);
+    backend_t *be = be_screen->be;
 
     // wait, why the fuck are these two lines not the same???
     //struct wlr_output *o = be_screen->output; // segfaults
@@ -92,9 +106,35 @@ static void handle_frame(struct wl_listener *l, void *data){
     wlr_output_make_current(o, &age);
     wlr_renderer_begin(r, o->width, o->height);
 
-    // render a blue square
+    // // render a blue square
     float color[4] = {0.0, 0.0, 0.5, 1.0};
     wlr_renderer_clear(r, color);
+
+    // iterate over all the surface the compositor
+    // TODO: track which surfaces are showing?
+    struct wl_resource *resource;
+    wl_resource_for_each(resource, &be->compositor->surface_resources){
+        struct wlr_surface *surface = wlr_surface_from_resource(resource);
+        if(!wlr_surface_has_buffer(surface)){
+            continue;
+        }
+        struct wlr_box render_box = {
+            .x = 20,
+            .y = 20,
+            .width = surface->current.width,
+            .height = surface->current.height,
+        };
+        float mat[16];
+        wlr_matrix_project_box((float*)&mat, &render_box,
+                surface->current.transform, 0, (float*)&o->transform_matrix);
+        struct wlr_texture *texture = wlr_surface_get_texture(surface);
+        wlr_render_texture_with_matrix(r, texture, (float*)&mat, 1.0f);
+
+        struct timespec now;
+        clock_gettime(CLOCK_REALTIME, &now);
+        wlr_surface_send_frame_done(surface, &now);
+
+    }
 
     // done rendering, commit buffer
     wlr_output_swap_buffers(o, NULL, NULL);
@@ -138,6 +178,9 @@ static be_screen_t *be_screen_new(backend_t *be, struct wlr_output *output){
         goto cu_listeners;
     }
 
+    // create a global.  Not honestly sure what this is good for.
+    wlr_output_create_global(output);
+
     return be_screen;
 
 cu_listeners:
@@ -163,6 +206,202 @@ static void handle_new_output(struct wl_listener *l, void *data){
 
 ///// End Backend Screen Functions
 
+
+///// Input Functions
+
+typedef struct {
+    backend_t *be;
+    struct wlr_input_device *device;
+    struct wl_listener key_listener;
+    struct wl_listener mod_listener;
+} keyboard_t;
+
+static void handle_key(struct wl_listener *l, void *data){
+    keyboard_t *kbd = wl_container_of(l, kbd, key_listener);
+    struct wlr_event_keyboard_key *event = data;
+    (void)event; (void)kbd;
+    logmsg("key\n");
+}
+
+static void handle_modifier(struct wl_listener *l, void *data){
+    keyboard_t *kbd = wl_container_of(l, kbd, mod_listener);
+    struct wlr_event_keyboard_modifier *event = data;
+    (void)event; (void)kbd;
+    logmsg("modifier\n");
+}
+
+static struct xkb_context *xkb_context = NULL;
+static struct xkb_keymap *xkb_keymap = NULL;
+
+keyboard_t *keyboard_new(backend_t *be, struct wlr_input_device *device){
+    logmsg("new keyboard\n");
+    keyboard_t *kbd = malloc(sizeof(*kbd));
+    // if(!kbd) // TODO: what to do if malloc fails?
+    *kbd = (keyboard_t){0};
+
+    device->data = kbd;
+    kbd->device = device;
+
+    // load a keyboard map (generated with `xkbcomp $DISPLAY xkb.dump`)
+    if(!xkb_context){
+        xkb_context = xkb_context_new(XKB_CONTEXT_NO_FLAGS);
+        FILE *f = fopen("xkb.dump", "r");
+        if(!f){
+            perror("fopen(xkb.dump)");
+        }else{
+            xkb_keymap = xkb_keymap_new_from_file(
+                    xkb_context, f, XKB_KEYMAP_FORMAT_TEXT_V1, 0);
+            fclose(f);
+        }
+    }
+
+
+    wlr_keyboard_set_keymap(device->keyboard, xkb_keymap);
+
+    kbd->key_listener.notify = handle_key;
+    wl_signal_add(&device->keyboard->events.key, &kbd->key_listener);
+
+    kbd->mod_listener.notify = handle_modifier;
+    wl_signal_add(&device->keyboard->events.modifiers, &kbd->mod_listener);
+
+    return kbd;
+}
+
+typedef struct {
+    backend_t *be;
+    struct wlr_input_device *device;
+    struct wl_listener button_listener;
+    struct wl_listener motion_listener;
+} pointer_t;
+
+static void handle_button(struct wl_listener *l, void *data){
+    pointer_t *ptr = wl_container_of(l, ptr, button_listener);
+    struct wlr_event_mouse_button *event = data;
+    (void)event; (void)ptr;
+    logmsg("button\n");
+}
+
+static void handle_motion(struct wl_listener *l, void *data){
+    pointer_t *ptr = wl_container_of(l, ptr, motion_listener);
+    struct wlr_event_mouse_motion *event = data;
+    (void)event; (void)ptr;
+    logmsg("motion\n");
+}
+
+pointer_t *pointer_new(backend_t *be, struct wlr_input_device *device){
+    logmsg("new pointer\n");
+    pointer_t *ptr = malloc(sizeof(*ptr));
+    // if(!ptr) // TODO: what to do if malloc fails?
+    *ptr = (pointer_t){0};
+
+    device->data = ptr;
+    ptr->device = device;
+
+    ptr->button_listener.notify = handle_button;
+    wl_signal_add(&device->pointer->events.button, &ptr->button_listener);
+
+    ptr->motion_listener.notify = handle_motion;
+    wl_signal_add(&device->pointer->events.motion_absolute,
+                  &ptr->motion_listener);
+
+    return ptr;
+}
+
+
+
+
+static void handle_new_input_device(struct wl_listener *l, void *data){
+    backend_t *be = wl_container_of(l, be, new_input_device);
+    struct wlr_input_device *device = data;
+
+    switch(device->type){
+        case WLR_INPUT_DEVICE_KEYBOARD:
+            logmsg("input: WLR_INPUT_DEVICE_KEYBOARD\n");
+            keyboard_new(be, device);
+            break;
+        case WLR_INPUT_DEVICE_POINTER:
+            logmsg("input: WLR_INPUT_DEVICE_POINTER\n");
+            pointer_new(be, device);
+            break;
+        case WLR_INPUT_DEVICE_TOUCH:
+            logmsg("input: WLR_INPUT_DEVICE_TOUCH\n");
+            break;
+        case WLR_INPUT_DEVICE_TABLET_TOOL:
+            logmsg("input: WLR_INPUT_DEVICE_TABLET_TOOL\n");
+            break;
+        case WLR_INPUT_DEVICE_TABLET_PAD:
+            logmsg("input: WLR_INPUT_DEVICE_TABLET_PAD\n");
+            break;
+        case WLR_INPUT_DEVICE_SWITCH:
+            logmsg("input: WLR_INPUT_DEVICE_SWITCH\n");
+            break;
+    }
+}
+
+///// End Input Functions
+
+
+///// Backend Window Functions
+
+struct be_window_t {
+    backend_t *be;
+    // base wl_surface, the first thing to be created
+    struct wlr_surface *wlr_surface;
+    struct wl_listener wlr_surface_destroyed;
+
+    // xdg_surface, extends a wl_surface
+    struct wlr_xdg_surface *xdg_surface;
+    struct wl_listener xdg_surface_destroyed;
+};
+
+static void be_window_free(be_window_t *be_window){
+    // don't need to remove destroy handlers
+    free(be_window);
+}
+
+static void handle_wlr_surface_destroyed(struct wl_listener *l, void *data){
+    (void)data;
+    be_window_t *be_window;
+    be_window = wl_container_of(l, be_window, wlr_surface_destroyed);
+    be_window_free(be_window);
+    logmsg("freed window (handle_wlr_surface_destroyed)!\n");
+}
+
+static be_window_t *be_window_new(backend_t *be,
+                                  struct wlr_surface *wlr_surface){
+    be_window_t *be_window = malloc(sizeof(*be_window));
+    if(!be_window) return NULL;
+    *be_window = (be_window_t){0};
+
+    be_window->be = be;
+    be_window->wlr_surface = wlr_surface;
+
+    // add destroy handler
+    be_window->wlr_surface_destroyed.notify = handle_wlr_surface_destroyed;
+    wl_signal_add(&wlr_surface->events.destroy,
+                  &be_window->wlr_surface_destroyed);
+
+    return be_window;
+}
+
+static void handle_new_surface(struct wl_listener *l, void *data){
+    struct wlr_surface *surface = data;
+    backend_t *be = wl_container_of(l, be, wlr_surface_created);
+    be_window_t *be_window = be_window_new(be, surface);
+    // TODO: how to close this surface on errors?
+    if(!be_window) return;
+
+    logmsg("new surface!\n");
+}
+
+static void handle_xdg_shell_new(struct wl_listener *l, void *data){
+    struct wlr_xdg_surface *surface = data;
+    backend_t *be = wl_container_of(l, be, xdg_shell_new_listener);
+    logmsg("new xdg surface!\n");
+    (void)surface;
+}
+
+///// End Backend Window Functions
 
 
 int be_handle_key(backend_t *be, uint32_t mods, uint32_t key,
@@ -194,12 +433,21 @@ void be_repaint(backend_t *be){
 }
 
 void backend_free(backend_t *be){
-    wlr_xdg_shell_v6_destroy(be->xdg_shell_v6);
-    wlr_compositor_destroy(be->compositor);
+    wl_list_remove(&be->wlr_surface_created.link);
+    wl_list_remove(&be->compositor_destroyed_listener.link);
+    wl_list_remove(&be->xdg_shell_new_listener.link);
+    wlr_xdg_shell_destroy(be->xdg_shell);
+    // this gets called during compositor_destroyed
+    // wlr_compositor_destroy(be->compositor);
     wl_list_remove(&be->new_output_listener.link);
     wlr_backend_destroy(be->wlr_backend);
     wl_display_destroy(be->display);
     free(be);
+}
+
+static void handle_compositor_destroyed(struct wl_listener *l, void *data){
+    backend_t *be = wl_container_of(l, be, compositor_destroyed_listener);
+    backend_free(be);
 }
 
 backend_t *backend_new(void){
@@ -216,6 +464,8 @@ backend_t *backend_new(void){
     // get the wayland backend
     be->wlr_backend = wlr_backend_autocreate(be->display, NULL);
     if(!be->wlr_backend) goto fail_display;
+    be->new_input_device.notify = handle_new_input_device;
+    wl_signal_add(&be->wlr_backend->events.new_input, &be->new_input_device);
 
     // get ready for some outputs
     wl_list_init(&be->be_screens);
@@ -229,19 +479,34 @@ backend_t *backend_new(void){
     if(!be->compositor) goto fail_wlr_backend;
 
     // add some interfaces
-    be->xdg_shell_v6 = wlr_xdg_shell_v6_create(be->display);
-    if(!be->xdg_shell_v6) goto fail_compositor;
+    be->xdg_shell = wlr_xdg_shell_create(be->display);
+    if(!be->xdg_shell) goto fail_compositor;
+    be->xdg_shell_new_listener.notify = handle_xdg_shell_new;
+    wl_signal_add(&be->xdg_shell->events.new_surface,
+                  &be->xdg_shell_new_listener);
 
     wl_display_init_shm(be->display);
 
     // become a proper wayland server
     be->socket = wl_display_add_socket_auto(be->display);
-    if(!be->socket) goto fail_xdg_shell_v6;
+    if(!be->socket) goto fail_xdg_shell;
+
+    // add some listeners
+    be->wlr_surface_created.notify = handle_new_surface;
+    wl_signal_add(&be->compositor->events.new_surface,
+                  &be->wlr_surface_created);
+    be->compositor_destroyed_listener.notify = handle_compositor_destroyed;
+    wl_signal_add(&be->compositor->events.destroy,
+                  &be->compositor_destroyed_listener);
 
     return be;
 
-fail_xdg_shell_v6:
-    wlr_xdg_shell_v6_destroy(be->xdg_shell_v6);
+// fail_listeners:
+//     wl_list_remove(&be->wlr_surface_created.link);
+//     wl_list_remove(&be->compositor_destroyed_listener.link);
+fail_xdg_shell:
+    wl_list_remove(&be->xdg_shell_new_listener.link);
+    wlr_xdg_shell_destroy(be->xdg_shell);
 fail_compositor:
     wlr_compositor_destroy(be->compositor);
 fail_wlr_backend:
@@ -261,7 +526,8 @@ int backend_run(backend_t *be){
 
     setenv("WAYLAND_DISPLAY", be->socket, true);
 
-    exec("weston-info > wifo");
+    // exec("weston-info > wifo");
+    exec("weston-terminal");
 
     wl_display_run(be->display);
     // TODO: check error
@@ -269,4 +535,5 @@ int backend_run(backend_t *be){
 }
 
 void backend_stop(backend_t *be){
+    wl_display_terminate(be->display);
 }
--- makefile
+++ makefile
@@ -1,11 +1,12 @@
 CC=gcc
 LD=ld
 
-PKGS=wlroots wayland-server
+PKGS=wlroots wayland-server xkbcommon
 CFLAGS+=-DWLR_USE_UNSTABLE
 
 CFLAGS+=-g
 CFLAGS+=-Wall -Wextra -std=c11
+CFLAGS+=-Iprotocol
 CFLAGS+=`pkg-config --cflags $(PKGS)`
 CFLAGS+=-fdiagnostics-color=always
 CFLAGS+=-Wno-unused-parameter
@@ -14,11 +15,32 @@ CFLAGS+=-Wno-unused-parameter
 LDFLAGS+=`pkg-config --libs $(PKGS)`
 LDFLAGS+=-lm
 
+XDG_DECORATION_XML=/usr/share/wayland-protocols/stable/xdg-shell/xdg-shell.xml
+
 all: clean venowm test_split
 
 test_split: split.o logmsg.o
 
-venowm:split.o screen.o workspace.o window.o logmsg.o bindings.o backend_wlroots.o
+backend_wlroots.o: protocols
+
+venowm:split.o \
+       screen.o \
+       workspace.o \
+       window.o \
+       logmsg.o \
+       bindings.o \
+       backend_wlroots.o
+
+protocol/xdg-shell-protocol.c: $(XDG_DECORATION_XML)
+	mkdir -p protocol
+	wayland-scanner private-code $< $@
+
+protocol/xdg-shell-protocol.h: $(XDG_DECORATION_XML)
+	mkdir -p protocol
+	wayland-scanner server-header $< $@
+
+protocols: protocol/xdg-shell-protocol.h \
+           protocol/xdg-shell-protocol.c
 
 clean:
-	rm -f *.o venowm test_split logmsg
+	rm -f *.o venowm test_split logmsg -r protocol
